fb72100971285b62d2825a92850324b991933b62
Alfinhi Hajid Dhia <85265678+AlfinhiHD@users.noreply.github.com>
2023-05-14 11:07:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Ini branch main yah')
+    res.end('Ini branch dev, branch dimana proses development dilakukan')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
